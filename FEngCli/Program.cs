fad885c7f9cc91f22f973c661be9f36ca734c264
leo <coderleo42+leocodes@gmail.com>
2020-11-21 12:43:40 -0500

    Add resource list printing

--- FEngCli/Program.cs
+++ FEngCli/Program.cs
@@ -36,12 +36,28 @@ namespace FEngCli
 
         private static void DumpPackageInfo(FrontendPackage package)
         {
+            for (var index = 0; index < package.ResourceRequests.Count; index++)
+            {
+                var resourceRequest = package.ResourceRequests[index];
+                Console.WriteLine("Resource request:");
+                Console.WriteLine("\tIndex  : {0}", index);
+                Console.WriteLine("\tName   : {0}", resourceRequest.Name);
+                Console.WriteLine("\tType   : {0}", resourceRequest.Type);
+                Console.WriteLine("\tFlags  : {0}", resourceRequest.Flags);
+                Console.WriteLine("\tHandle : {0}", resourceRequest.Handle);
+                Console.WriteLine("\tID     : {0:X8}", resourceRequest.ID);
+            }
+
             foreach (var frontendObject in package.Objects) DumpObjectInfo(frontendObject);
         }
 
         private static void DumpObjectInfo(FrontendObject frontendObject)
         {
             Console.WriteLine(frontendObject);
+            Console.WriteLine("\tGUID     : 0x{0:X}", frontendObject.Guid);
+            Console.WriteLine("\tFlags    : {0}", frontendObject.Flags);
+            if (frontendObject.Parent != null)
+                Console.WriteLine("\tParent   : 0x{0:X}", frontendObject.Parent.Guid);
             Console.WriteLine("\tPosition : {0}", frontendObject.Position);
             Console.WriteLine("\tSize     : {0}", frontendObject.Size);
             Console.WriteLine("\tExtent   : {0}",
@@ -52,6 +68,7 @@ namespace FEngCli
             Console.WriteLine("\tRotation : {0}", frontendObject.Rotation);
             Console.WriteLine("\tColor    : {0}", frontendObject.Color);
             Console.WriteLine("\tType     : {0}", frontendObject.Type);
+            Console.WriteLine("\tResource : {0}", frontendObject.ResourceIndex);
 
             switch (frontendObject)
             {
