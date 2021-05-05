62fd6688f2eb71cc5cc7b82341e9421a1f302430
fridtjof <fridtjof@das-labor.org>
2021-05-06 00:08:08 +0200

    FEngViewer: initial GLRenderControl

---
+++ FEngViewer/GLRenderControl.Designer.cs
@@ -0,0 +1,66 @@
+﻿
+namespace FEngViewer
+{
+    partial class GLRenderControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.glControl = new OpenTK.WinForms.GLControl();
+            this.SuspendLayout();
+            // 
+            // glControl
+            // 
+            this.glControl.API = OpenTK.Windowing.Common.ContextAPI.OpenGL;
+            this.glControl.APIVersion = new System.Version(3, 3, 0, 0);
+            this.glControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.glControl.Flags = OpenTK.Windowing.Common.ContextFlags.Default;
+            this.glControl.IsEventDriven = true;
+            this.glControl.Location = new System.Drawing.Point(0, 0);
+            this.glControl.Name = "glControl";
+            this.glControl.Profile = OpenTK.Windowing.Common.ContextProfile.Core;
+            this.glControl.Size = new System.Drawing.Size(640, 480);
+            this.glControl.TabIndex = 0;
+            this.glControl.Text = "glControl1";
+            this.glControl.Load += new System.EventHandler(this.glControl_Load);
+            this.glControl.Paint += new System.Windows.Forms.PaintEventHandler(this.glControl_Paint);
+            // 
+            // GLRenderControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.glControl);
+            this.Name = "GLRenderControl";
+            this.Size = new System.Drawing.Size(640, 480);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private OpenTK.WinForms.GLControl glControl;
+    }
+}
