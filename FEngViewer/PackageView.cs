6e347935599166b6ec34e793e84ad9bca25f672f
leo <coderleo42+leocodes@gmail.com>
2021-05-07 10:27:34 -0400

    a bit of PackageView cleanup

--- FEngViewer/PackageView.cs
+++ FEngViewer/PackageView.cs
@@ -10,7 +10,6 @@ using FEngLib.Data;
 using FEngRender.Data;
 using FEngViewer.Properties;
 using JetBrains.Annotations;
-using SharpGL;
 
 namespace FEngViewer
 {
@@ -111,7 +110,7 @@ namespace FEngViewer
             return objTreeNode;
         }
 
-        private static TreeNode CreateScriptTreeNode(TreeNodeCollection collection, FrontendScript script)
+        private static void CreateScriptTreeNode(TreeNodeCollection collection, FrontendScript script)
         {
             var node = collection.Add(script.Name ?? $"0x{script.Id:X}");
             // ReSharper disable once LocalizableElement
@@ -144,8 +143,6 @@ namespace FEngViewer
                 var trackNode = node.Nodes.Add(trackName);
                 trackNode.ImageKey = trackNode.SelectedImageKey = "TreeItem_ScriptTrack";
             }
-
-            return node;
         }
 
         private static FrontendPackage LoadPackageFromChunk(string path)
@@ -197,7 +194,7 @@ namespace FEngViewer
         private class Options
         {
             [Option('i', "input")]
-            public string InputFile { get; set; }
+            public string InputFile { get; [UsedImplicitly] set; }
         }
 
         private void OpenFileMenuItem_Click(object sender, EventArgs e)
@@ -255,97 +252,5 @@ namespace FEngViewer
                 Render();
             }
         }
-
-        private float rotCube = 0;
-
-        /*private void openglControl1_OpenGLDraw(object sender, RenderEventArgs args)
-        {
-            OpenGL gl = this.openglControl1.OpenGL;
-            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
-
-            gl.LoadIdentity();
-            gl.Translate(1.5f, 0.0f, -4);				// Move Right And Into The Screen
-
-            gl.Rotate(rotCube += 3, 1.0f, 1.0f, 1.0f);			// Rotate The Cube On X, Y & Z
-
-            gl.Begin(OpenGL.GL_QUADS);					// Start Drawing The Cube
-
-            gl.Color(0.0f, 1.0f, 0.0f);			// Set The Color To Green
-            gl.Vertex(1.0f, 1.0f, -1.0f);			// Top Right Of The Quad (Top)
-            gl.Vertex(-1.0f, 1.0f, -1.0f);			// Top Left Of The Quad (Top)
-            gl.Vertex(-1.0f, 1.0f, 1.0f);			// Bottom Left Of The Quad (Top)
-            gl.Vertex(1.0f, 1.0f, 1.0f);			// Bottom Right Of The Quad (Top)
-
-
-            gl.Color(1.0f, 0.5f, 0.0f);			// Set The Color To Orange
-            gl.Vertex(1.0f, -1.0f, 1.0f);			// Top Right Of The Quad (Bottom)
-            gl.Vertex(-1.0f, -1.0f, 1.0f);			// Top Left Of The Quad (Bottom)
-            gl.Vertex(-1.0f, -1.0f, -1.0f);			// Bottom Left Of The Quad (Bottom)
-            gl.Vertex(1.0f, -1.0f, -1.0f);			// Bottom Right Of The Quad (Bottom)
-
-            gl.Color(1.0f, 0.0f, 0.0f);			// Set The Color To Red
-            gl.Vertex(1.0f, 1.0f, 1.0f);			// Top Right Of The Quad (Front)
-            gl.Vertex(-1.0f, 1.0f, 1.0f);			// Top Left Of The Quad (Front)
-            gl.Vertex(-1.0f, -1.0f, 1.0f);			// Bottom Left Of The Quad (Front)
-            gl.Vertex(1.0f, -1.0f, 1.0f);			// Bottom Right Of The Quad (Front)
-
-            gl.Color(1.0f, 1.0f, 0.0f);			// Set The Color To Yellow
-            gl.Vertex(1.0f, -1.0f, -1.0f);			// Bottom Left Of The Quad (Back)
-            gl.Vertex(-1.0f, -1.0f, -1.0f);			// Bottom Right Of The Quad (Back)
-            gl.Vertex(-1.0f, 1.0f, -1.0f);			// Top Right Of The Quad (Back)
-            gl.Vertex(1.0f, 1.0f, -1.0f);			// Top Left Of The Quad (Back)
-
-            gl.Color(0.0f, 0.0f, 1.0f);			// Set The Color To Blue
-            gl.Vertex(-1.0f, 1.0f, 1.0f);			// Top Right Of The Quad (Left)
-            gl.Vertex(-1.0f, 1.0f, -1.0f);			// Top Left Of The Quad (Left)
-            gl.Vertex(-1.0f, -1.0f, -1.0f);			// Bottom Left Of The Quad (Left)
-            gl.Vertex(-1.0f, -1.0f, 1.0f);			// Bottom Right Of The Quad (Left)
-
-            gl.Color(1.0f, 0.0f, 1.0f);			// Set The Color To Violet
-            gl.Vertex(1.0f, 1.0f, -1.0f);			// Top Right Of The Quad (Right)
-            gl.Vertex(1.0f, 1.0f, 1.0f);			// Top Left Of The Quad (Right)
-            gl.Vertex(1.0f, -1.0f, 1.0f);			// Bottom Left Of The Quad (Right)
-            gl.Vertex(1.0f, -1.0f, -1.0f);			// Bottom Right Of The Quad (Right)
-            gl.End();                       // Done Drawing The Q
-
-            gl.Flush();
-
-            //gl.Translate(-1.5f, 0.0f, -6.0f);				// Move Left And Into The Screen
-
-            //gl.Rotate(0, 0.0f, 1.0f, 0.0f);				// Rotate The Pyramid On It's Y Axis
-
-            //gl.Begin(OpenGL.GL_TRIANGLES);					// Start Drawing The Pyramid
-
-            //gl.Color(1.0f, 0.0f, 0.0f);			// Red
-            //gl.Vertex(0.0f, 1.0f, 0.0f);			// Top Of Triangle (Front)
-            //gl.Color(0.0f, 1.0f, 0.0f);			// Green
-            //gl.Vertex(-1.0f, -1.0f, 1.0f);			// Left Of Triangle (Front)
-            //gl.Color(0.0f, 0.0f, 1.0f);			// Blue
-            //gl.Vertex(1.0f, -1.0f, 1.0f);			// Right Of Triangle (Front)
-
-            //gl.Color(1.0f, 0.0f, 0.0f);			// Red
-            //gl.Vertex(0.0f, 1.0f, 0.0f);			// Top Of Triangle (Right)
-            //gl.Color(0.0f, 0.0f, 1.0f);			// Blue
-            //gl.Vertex(1.0f, -1.0f, 1.0f);			// Left Of Triangle (Right)
-            //gl.Color(0.0f, 1.0f, 0.0f);			// Green
-            //gl.Vertex(1.0f, -1.0f, -1.0f);			// Right Of Triangle (Right)
-
-            //gl.Color(1.0f, 0.0f, 0.0f);			// Red
-            //gl.Vertex(0.0f, 1.0f, 0.0f);			// Top Of Triangle (Back)
-            //gl.Color(0.0f, 1.0f, 0.0f);			// Green
-            //gl.Vertex(1.0f, -1.0f, -1.0f);			// Left Of Triangle (Back)
-            //gl.Color(0.0f, 0.0f, 1.0f);			// Blue
-            //gl.Vertex(-1.0f, -1.0f, -1.0f);			// Right Of Triangle (Back)
-
-            //gl.Color(1.0f, 0.0f, 0.0f);			// Red
-            //gl.Vertex(0.0f, 1.0f, 0.0f);			// Top Of Triangle (Left)
-            //gl.Color(0.0f, 0.0f, 1.0f);			// Blue
-            //gl.Vertex(-1.0f, -1.0f, -1.0f);			// Left Of Triangle (Left)
-            //gl.Color(0.0f, 1.0f, 0.0f);			// Green
-            //gl.Vertex(-1.0f, -1.0f, 1.0f);			// Right Of Triangle (Left)
-            //gl.End();
-
-            gl.Flush();
-        }*/
     }
 }
