db8baba8a8590c9f7072b5f54a03256bf3a363eb
fridtjof <fridtjof@das-labor.org>
2022-02-07 04:07:41 +0100

    FrontendChunkWriter: general object data done, missing some misc stuff, mostly string tags.

---
+++ FEngLib/Utils/BinaryWriterChunkExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace FEngLib.Utils;
+
+public static class BinaryWriterChunkExtensions
+{
+    internal static void WriteChunk(this BinaryWriter target, FrontendChunkType id, Action<BinaryWriter> chunkWriter)
+    {
+        using var ms = new MemoryStream();
+        using var bw = new BinaryWriter(ms);
+        
+        chunkWriter(bw);
+
+        target.WriteEnum(id);
+        target.Write((int) ms.Length);
+        ms.WriteTo(target.BaseStream);
+    }
+    
+    internal static void WriteTag(this BinaryWriter target, FrontendTagType id, Action<BinaryWriter> chunkWriter)
+    {
+        using var ms = new MemoryStream();
+        using var bw = new BinaryWriter(ms);
+        
+        chunkWriter(bw);
+
+        target.WriteEnum(id);
+        target.Write((short) ms.Length);
+        ms.WriteTo(target.BaseStream);
+    }
+}
