ca0b05b75c41f3384376c7748983046fa0eac99b
fridtjof <fridtjof@das-labor.org>
2022-02-07 03:14:39 +0100

    FEngLib: annotate tag values with their character representation

--- FEngLib/FrontendTagType.cs
+++ FEngLib/FrontendTagType.cs
@@ -3,43 +3,43 @@
 public enum FrontendTagType : ushort
 {
     // part of ObjD
-    ObjectType = 0x744F,
-    ObjectHash = 0x684F,
-    ObjectReference = 0x504F,
-    ImageInfo = 0x6649,
-    ObjectData = 0x4153,
-    ObjectParent = 0x4150,
-    StringBufferLength = 0x6253,
-    StringBufferText = 0x7453,
-    StringBufferFormatting = 0x6A53,
-    StringBufferLeading = 0x6C53,
-    StringBufferMaxWidth = 0x7753,
-    StringBufferLabelHash = 0x4853,
-    MultiImageTexture1 = 0x314D,
-    MultiImageTexture2 = 0x324D,
-    MultiImageTexture3 = 0x334D,
-    MultiImageTextureFlags1 = 0x614D,
-    MultiImageTextureFlags2 = 0x624D,
-    MultiImageTextureFlags3 = 0x634D,
-    ObjectName = 0x6E4F,
-    StringBufferLabel = 0x4C53,
+    ObjectType = 0x744F, // Ot
+    ObjectHash = 0x684F, // Oh
+    ObjectReference = 0x504F, // OP
+    ImageInfo = 0x6649, // If
+    ObjectData = 0x4153, // SA
+    ObjectParent = 0x4150, // PA
+    StringBufferLength = 0x6253, // Sb
+    StringBufferText = 0x7453, // St
+    StringBufferFormatting = 0x6A53, // Sj
+    StringBufferLeading = 0x6C53, // Sl
+    StringBufferMaxWidth = 0x7753, // Sw
+    StringBufferLabelHash = 0x4853, // SH
+    MultiImageTexture1 = 0x314D, // M1
+    MultiImageTexture2 = 0x324D, // M2
+    MultiImageTexture3 = 0x334D, // M3
+    MultiImageTextureFlags1 = 0x614D, // Ma
+    MultiImageTextureFlags2 = 0x624D, // Mb
+    MultiImageTextureFlags3 = 0x634D, // Mc
+    ObjectName = 0x6E4F, // On
+    StringBufferLabel = 0x4C53, // SL
 
     // part of Scrp
-    ScriptHeader = 0x6853,
-    ScriptChain = 0x6353,
-    ScriptKeyTrack = 0x4946,
-    ScriptTrackOffset = 0x6F54,
-    ScriptKeyNode = 0x644B,
-    ScriptEvents = 0x5645,
-    ScriptName = 0x6E53,
+    ScriptHeader = 0x6853, // SD
+    ScriptChain = 0x6353, // Sc
+    ScriptKeyTrack = 0x4946, // FI
+    ScriptTrackOffset = 0x6F54, // To
+    ScriptKeyNode = 0x644B, // Kd
+    ScriptEvents = 0x5645, // EV
+    ScriptName = 0x6E53, // Sn
 
     // part of Message related chunks (MsgR, PkgR, Targ)
-    MessageResponseInfo = 0x694D,
-    MessageResponseCount = 0x434D,
-    ResponseId = 0x6952,
-    ResponseIntParam = 0x7552,
-    ResponseStringParam = 0x7352,
-    ResponseTarget = 0x7452,
-    MessageTargetCount = 0x6354,
-    MessageTargetList = 0x744D,
+    MessageResponseInfo = 0x694D, // Mi
+    MessageResponseCount = 0x434D, // MC
+    ResponseId = 0x6952, // Ri
+    ResponseIntParam = 0x7552, // Ru
+    ResponseStringParam = 0x7352, // Rs
+    ResponseTarget = 0x7452, // Rt
+    MessageTargetCount = 0x6354, // Tc
+    MessageTargetList = 0x744D, // Mt
 }
