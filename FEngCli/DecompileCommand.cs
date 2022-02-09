327d1e22fa2269fe904badbcb9f5de8ea88f8900
leo <coderleo42+leocodes@gmail.com>
2022-02-08 20:03:06 -0500

    JSON stuff + test bench update

---
+++ FEngCli/DecompileCommand.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using CommandLine;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace FEngCli;
+
+[Verb("decompile")]
+public class DecompileCommand : BaseCommand
+{
+    [Option('i')] public string InputPath { get; set; }
+
+    [Option('o')] public string OutputPath { get; set; }
+
+    public override int Execute()
+    {
+        var package = PackageLoader.Load(InputPath);
+
+        File.WriteAllText(OutputPath, JsonConvert.SerializeObject(package, new JsonSerializerSettings
+        {
+            Formatting = Formatting.Indented,
+            Converters = new List<JsonConverter>
+            {
+                new StringEnumConverter()
+            },
+            TypeNameHandling = TypeNameHandling.Auto,
+            ReferenceLoopHandling = ReferenceLoopHandling.Error,
+            PreserveReferencesHandling = PreserveReferencesHandling.Objects
+        }));
+
+        return 0;
+    }
+}
---
+++ FEngCli/PackageLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using FEngLib.Packages;
+
+namespace FEngCli;
+
+public static class PackageLoader
+{
+    public static Package Load(string path)
+    {
+        using var fs = new FileStream(path, FileMode.Open);
+        using var fr = new BinaryReader(fs);
+        var marker = fr.ReadUInt32();
+        switch (marker)
+        {
+            case 0x30203:
+                fs.Seek(0x10, SeekOrigin.Begin);
+                break;
+            case 0xE76E4546:
+                fs.Seek(0x8, SeekOrigin.Begin);
+                break;
+            default:
+                throw new InvalidDataException($"Invalid FEng chunk file: {path}");
+        }
+
+        using var ms = new MemoryStream();
+        fs.CopyTo(ms);
+        ms.Position = 0;
+
+        using var mr = new BinaryReader(ms);
+        return new FrontendPackageLoader().Load(mr);
+    }
+}
